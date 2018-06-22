a176b743e3960ef76907910c8079c10c2336dbfa
murrs <40471448+murrs@users.noreply.github.com>
2018-06-22 07:25:59 -0700

    Add files via upload

---
+++ shiny_two_way_tables.R
@@ -0,0 +1,96 @@
+## app.R ##
+library(shinydashboard)
+library(shiny)
+library(data.table)
+library(survey)
+
+censusResults = fread("/home/aaron/Documents/census/Data/main_results_2017/Clean2017CensusFulltabMar2018.csv",
+                      sep = "\t", fill = TRUE, na.strings=c(""," ","NA"))
+censusResults$weightbmorg2[is.na(censusResults$weightbmorg2)] = 0
+censusDesign = svydesign(ids = ~id, weights = ~weightbmorg2,
+                         data = censusResults)
+
+weighted_table = function(rowvar, colvar, weights){
+  rowLevels = unique(rowvar)[order(unique(rowvar))]
+  rowLevels = rowLevels[!is.na(rowLevels)]
+  colLevels = unique(colvar)[order(unique(colvar))]
+  colLevels = colLevels[!is.na(colLevels)]
+  #  wtab = matrix(0, length(rowLevels), length(colLevels))
+  wtab = sapply(1:length(colLevels), function(c, w, rl, cl, rv, cv){
+    sapply(1:length(rowLevels), function (r, c, w, rl, cl, rv, cv){
+      sum(w[rowvar == rowLevels[r] & colvar == colLevels[c]],
+          na.rm = TRUE)
+    }, w = w, c = c, rl = rl, cl = cl, rv = rv, cv = cv)
+  }, w = weights, rl = rowLevels, cl = colLevels, rv = rowvar, cv = colvar)
+  colnames(wtab) = colLevels
+  rownames(wtab) = rowLevels
+  return(wtab)
+}
+
+
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Inputs", icon = icon("bar-chart-o"),
+             # Input directly under menuItem
+             selectInput("rowvar", "Row Variable",
+                         choices = c("Gender" = "gender", "Race" = "ethno",
+                                     "Age" = "agegr4", "Virgin" = "firstyear"),
+                         width = '98%'),
+             
+             # Input inside of menuSubItem
+             selectizeInput("colvar", "Column Variable",
+                            choices = names(censusResults),
+                            multiple = FALSE, selected = "completed",
+                            width = '98%')
+    )
+  )),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(dataTableOutput('table1'))
+    ),
+    fluidRow(
+      box(dataTableOutput('table2'))
+    ),
+    fluidRow(
+      box(dataTableOutput('table3'))
+    )
+  )
+)
+
+server <- function(input, output) {
+  censusResults = fread("Data\\main_results_2016\\Clean2016CensusFullFeb2017.csv",
+                        na.strings=c(""," ","NA"))
+  censusResults$weightbmorg2[is.na(censusResults$weightbmorg2)] = 0
+  censusDesign = svydesign(ids = ~id, weights = ~weightbmorg2,
+                           data = censusResults)
+  
+  
+  
+  output$table1 <- renderDataTable({
+    v1 <- censusResults[[input$rowvar]]
+    v2 <- censusResults[[input$colvar]]
+    weights = censusResults$weightbmorg2
+    prop.table(weighted_table(v1, v2, weights))
+  }
+  )
+  output$table2 <- renderDataTable({
+    v1 <- censusResults[[input$rowvar]]
+    v2 <- censusResults[[input$colvar]]
+    weights = censusResults$weightbmorg2
+    prop.table(weighted_table(v1, v2, weights), 1)
+  }
+  )
+  output$table3 <- renderDataTable({
+    v1 <- censusResults[[input$rowvar]]
+    v2 <- censusResults[[input$colvar]]
+    weights = censusResults$weightbmorg2
+    prop.table(weighted_table(v1, v2, weights), 2)
+  }
+  )
+}
+
+shinyApp(ui, server)
